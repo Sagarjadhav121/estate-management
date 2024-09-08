bdb6cd6c4b4054188fea7e9decc8cf629a9c12dc
Sagar Jadhav <jadhavsagar121@gmail.com>
2024-09-08 17:04:55 +0530

    database connection setup

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 const App = express();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("DB connected successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 App.listen(3000, () => {
   console.log("server running on 3000 !!!");
